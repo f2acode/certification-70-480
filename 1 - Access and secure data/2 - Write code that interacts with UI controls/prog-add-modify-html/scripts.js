fc4ee941745100412fa283a48a48256cad611509
Felipe augusto de almeida <augustto.felipe@hotmail.com>
2017-10-01 01:05:38 -0300

    inserting firsts javascript studies

--- 1 - Access and secure data/2 - Write code that interacts with UI controls/prog-add-modify-html/scripts.js
+++ 1 - Access and secure data/2 - Write code that interacts with UI controls/prog-add-modify-html/scripts.js
@@ -1,1 +1,39 @@
-document.getElementById("accesskey").accessKey = "o";
+window.onload = function () {
+
+    //element.accessKey
+    document.getElementById("accessKey").accessKey = "o";
+
+    //element.addEventListener()
+    document.getElementById("eventListener").addEventListener("click", function () {
+        alert("Event listener activated!");
+    });
+
+    //element.appendChild()
+    var paragraph = document.createElement("P");
+    var textParagraph = document.createTextNode("Append child added inside");
+    paragraph.appendChild(textParagraph);
+    document.getElementById("appendChild").appendChild(paragraph);
+
+    //element.attributes
+    paragraph = document.createElement("P");
+    var attributes = document.getElementById("attributes").attributes;
+    textParagraph = document.createTextNode("Atributes: " + attributes
+        + " Size: " + attributes.length);
+    paragraph.appendChild(textParagraph);
+    document.getElementById("attributes").appendChild(paragraph);
+
+    //element.blur()
+    document.getElementById("blur").focus();
+    setTimeout(function () {
+        document.getElementById("blur").blur();
+    }, 2000);
+
+    //element.childElementCount
+    paragraph = document.createElement("P");
+    var childElements = document.getElementById("childElementCount").childElementCount;
+    textParagraph = document.createTextNode("Child elements: " + childElements);
+    paragraph.appendChild(textParagraph);
+    document.getElementById("childElementCount").appendChild(paragraph);
+
+    //
+};
